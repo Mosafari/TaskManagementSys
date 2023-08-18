457581c1d458107a8af4e705f10d1cafe0103262
mosafari <m.safari2680@gmail.com>
2023-08-18 18:22:55 +0330

    add project (spring initializer)

---
+++ taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/TaskmanagementsysApplication.java
@@ -0,0 +1,13 @@
+package com.jproject.taskmanagementsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementsysApplication.class, args);
+	}
+
+}
---
+++ taskmanagementsys/src/test/java/com/jproject/taskmanagementsys/TaskmanagementsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jproject.taskmanagementsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
