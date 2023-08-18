fef52cca7a1d02d6ab32b7f0b97b8b33c9f2c581
mosafari <m.safari2680@gmail.com>
2023-08-18 19:30:27 +0330

    add constructor for model

--- taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/Task.java
+++ taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/Task.java
@@ -1,5 +1,31 @@
 package com.jproject.taskmanagementsys;
 
-public class Task {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String title;
+    private String description;
+    private String status;
+
+    // Constructors, getters, setters
+//    Constructors are used to create instances of a class.
+ // No-argument constructor (required by JPA)
+    public Task() {
+    }
+
+    // Parameterized constructor
+//    The parameterized constructor allows you to conveniently initialize the attributes when creating instances.
+    public Task(String title, String description, String status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
 }
