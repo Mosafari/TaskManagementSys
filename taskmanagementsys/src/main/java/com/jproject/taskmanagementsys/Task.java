053ca1e86dd1527746406a952db957fdf3b0c162
mosafari <m.safari2680@gmail.com>
2023-08-18 19:31:32 +0330

    add getter for model

--- taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/Task.java
+++ taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/Task.java
@@ -28,4 +28,22 @@ public class Task {
         this.description = description;
         this.status = status;
     }
+    
+//    Getter Methods:
+//    Getter methods allow you to access the values of the attributes of your entity objects.
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
