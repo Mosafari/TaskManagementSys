f2b704f016904d1406bd12cd09edf357168866f8
mosafari <m.safari2680@gmail.com>
2023-08-19 20:28:54 +0330

    adding TaskService (business logic)

---
+++ taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/service/TaskService.java
@@ -0,0 +1,45 @@
+package com.jproject.taskmanagementsys.service;
+
+import com.jproject.taskmanagementsys.Task;
+import com.jproject.taskmanagementsys.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    // Add methods to handle business logic (CRUD operations, etc.)
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long taskId) {
+        return taskRepository.findById(taskId).orElse(null);
+    }
+
+    public Task updateTask(Long taskId, Task updatedTask) {
+        Task existingTask = getTaskById(taskId);
+        if (existingTask != null) {
+            existingTask.setTitle(updatedTask.getTitle());
+            existingTask.setDescription(updatedTask.getDescription());
+            existingTask.setStatus(updatedTask.getStatus());
+            return taskRepository.save(existingTask);
+        }
+        return null;
+    }
+
+    public void deleteTask(Long taskId) {
+        taskRepository.deleteById(taskId);
+    }
+}
