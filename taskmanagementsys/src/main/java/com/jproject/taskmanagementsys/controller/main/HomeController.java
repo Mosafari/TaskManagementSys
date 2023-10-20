71a4f629d442116edd2d3702605cca5fb34db557
mosafari <m.safari2680@gmail.com>
2023-10-20 18:42:45 +0330

    adding style / show and del tasks

--- taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/controller/main/HomeController.java
+++ taskmanagementsys/src/main/java/com/jproject/taskmanagementsys/controller/main/HomeController.java
@@ -2,17 +2,15 @@ package com.jproject.taskmanagementsys.controller.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jproject.taskmanagementsys.model.Employee;
+import com.jproject.taskmanagementsys.model.Task;
 import com.jproject.taskmanagementsys.service.EmpService;
 import com.jproject.taskmanagementsys.service.TaskService;
 
@@ -70,11 +68,52 @@ public class HomeController {
 	@PostMapping("/employee/add")
 	public String addEmployee(@ModelAttribute Employee emp, Model model) {
 		if (emp.getEmail()=="") {
-			return "createemployee";
+			return "redirect:/home/employee/create";
 		}
 		else {
 		empService.create(emp);
 		return empoloyeesShow(model);
+		}
+	}
+	
+	@GetMapping("/tasks")
+	public String tasksShow(Model model) {
+		model.addAttribute("tasks",taskService.getAllTasks());
+		return "tasks.html";
+	}
+	
+	@GetMapping("/task/{id}/edit")
+	public String editTask(Model model, @PathVariable("id") Long id) {
+		model.addAttribute("task", taskService.getTaskById(id));
+		model.addAttribute("id", id);
+		return "edittask";
+	}
+	
+	@PostMapping("/task/update/{id}")
+	public String updateTask(@PathVariable("id") Long id, @ModelAttribute Task task, Model model) {
+		if (task.getDescription()=="") {
+			return "createtask";
+		}
+		else {
+		taskService.updateTask(id, task);
+		return tasksShow(model);
+		}
+	}
+	
+	@GetMapping("/task/create")
+	public String createTaskPage( Model model) {
+	
+		return "createtask" ;
+	}
+	
+	@PostMapping("/task/add")
+	public String addTask(@ModelAttribute Task task, Model model) {
+		if (task.getDescription()=="") {
+			return "redirect:/home/employee/create";
+		}
+		else {
+		taskService.createTask(task);
+		return tasksShow(model);
 		}
 	}
 }
